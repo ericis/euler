85ea06b37b639a6e0fcc83f99d490e0a9179223e
Eric Swanson <eric.ca.swanson@aa.com>
2020-01-03 12:35:24 -0500

    feat(001): optimize with algorithm

--- 001/index.js
+++ 001/index.js
@@ -1,13 +1,17 @@
 
 module.exports = (value) => {
 
-    let sum = 0, n = value
+    let n = value, sum3 = 0, sum5 = 0, sum15 = 0
 
-    // brute force it
+    // optimize with an algorithm
+    // sum[3 or 5](n) = sum[3](n) + sum[5](n) - sum[15](n)
     while (n--) {
-        if (n % 3 === 0 || n % 5 === 0)
-            sum += n
+        if (n % 3 === 0) sum3 += n
+        if (n % 5 === 0) sum5 += n
+        if (n % 15 === 0) sum15 += n
     }
+
+    const sum = (sum3 + sum5) - sum15
 
     return sum
 }
