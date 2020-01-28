a72bbf257a8a395bb63d5d8813d96786ef65e9d0
Eric Swanson <eric.ca.swanson@aa.com>
2020-01-28 01:13:35 -0600

    feat(002): every 3rd term optimizes the algo to count every 3rd term (evens in fib seq) issue #2

--- src/002/index.js
+++ src/002/index.js
@@ -6,15 +6,16 @@ const euler = (upperBoundary) => {
 
         // NOTE: a recursive solution was nearly just as fast, but this is MUCH easier to read and maintain
         let total = 0
-        let x = 1, y = 1
+        let x = 1, y = 1, z = 2
 
+        // *every 3rd term is even, so it's not necessary to check for evens
         do {
-            // console.log('x: %d, y: %d', x, y)
-            if (x % 2 === 0) total += x
-            const z = x
-            x += y
-            y = z
-        } while(x <= upperBoundary)
+            // console.log('x: %d, y: %d, count: %d', x, y, termCount)
+            total += z
+            x = y + z // 1st: next term in seq
+            y = z + x // 2nd: next term after new 'x'
+            z = x + y // 3rd: next term after new 'x' and 'y'
+        } while(z <= upperBoundary)
 
         return total
     })
@@ -27,17 +28,18 @@ const freeCodeCampVersion = (termLimit) => {
     const sum = perf.measure('002', () => {
 
         // NOTE: a recursive solution was nearly just as fast, but this is MUCH easier to read and maintain
-        let termCount = 1
         let total = 0
-        let x = 1, y = 1
+        let x = 1, y = 1, z = 2
+        let termCount = 2
 
+        // *every 3rd term is even, so it's not necessary to check for evens
         do {
             // console.log('x: %d, y: %d, count: %d', x, y, termCount)
-            if (x % 2 === 0) total += x
-            const z = x
-            x += y
-            y = z
-            termCount++
+            total += z
+            x = y + z // 1st: next term in seq
+            y = z + x // 2nd: next term after new 'x'
+            z = x + y // 3rd: next term after new 'x' and 'y'
+            termCount += 3
         } while(termCount <= termLimit)
 
         return total
