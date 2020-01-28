c9dce877b0ab0157944b151983a4f81629a3d8cb
Eric Swanson <eric.ca.swanson@aa.com>
2020-01-28 01:21:59 -0600

    feat(002): refactor for shared algorithm

--- src/002/index.js
+++ src/002/index.js
@@ -1,23 +1,29 @@
 const perf = require('../perf')
 
+const algo = (continueCb) => {
+
+    // NOTE: a recursive solution was nearly just as fast, but this is MUCH easier to read and maintain
+    let total = 0
+    let x = 1, y = 1, z = 2
+    let keepGoing = true
+
+    // *every 3rd term is even, so it's not necessary to check for evens
+    do {
+        // console.log('x: %d, y: %d, count: %d', x, y, termCount)
+        total += z
+        x = y + z // 1st: next term in seq
+        y = z + x // 2nd: next term after new 'x'
+        z = x + y // 3rd: next term after new 'x' and 'y'
+        keepGoing = continueCb(z)
+    } while(keepGoing)
+
+    return total
+}
+
 const euler = (upperBoundary) => {
 
     const sum = perf.measure('002', () => {
-
-        // NOTE: a recursive solution was nearly just as fast, but this is MUCH easier to read and maintain
-        let total = 0
-        let x = 1, y = 1, z = 2
-
-        // *every 3rd term is even, so it's not necessary to check for evens
-        do {
-            // console.log('x: %d, y: %d, count: %d', x, y, termCount)
-            total += z
-            x = y + z // 1st: next term in seq
-            y = z + x // 2nd: next term after new 'x'
-            z = x + y // 3rd: next term after new 'x' and 'y'
-        } while(z <= upperBoundary)
-
-        return total
+        return algo(z => z <= upperBoundary)
     })
 
     return sum
@@ -27,22 +33,12 @@ const freeCodeCampVersion = (termLimit) => {
 
     const sum = perf.measure('002', () => {
 
-        // NOTE: a recursive solution was nearly just as fast, but this is MUCH easier to read and maintain
-        let total = 0
-        let x = 1, y = 1, z = 2
         let termCount = 2
 
-        // *every 3rd term is even, so it's not necessary to check for evens
-        do {
-            // console.log('x: %d, y: %d, count: %d', x, y, termCount)
-            total += z
-            x = y + z // 1st: next term in seq
-            y = z + x // 2nd: next term after new 'x'
-            z = x + y // 3rd: next term after new 'x' and 'y'
+        return algo(z => {
             termCount += 3
-        } while(termCount <= termLimit)
-
-        return total
+            return termCount <= termLimit
+        })
     })
 
     return sum
